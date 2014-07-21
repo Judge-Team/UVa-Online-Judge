b900c494abc3eadc24a8176e5ae8437ee2821013
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2014-07-21 22:08:53 +0800

    Finish 103

--- src/103.c
+++ src/103.c
@@ -42,6 +42,14 @@ int compare_size(void *x, void *y)
     return 0;
 }
 
+int compare_id(void *x, void *y)
+{
+    struct Box *a = (struct Box *) x;
+    struct Box *b = (struct Box *) y;
+
+    return a->id - b->id;
+}
+
 int is_stackable(struct Box *x, struct Box *y)
 {
     int i;
@@ -64,6 +72,8 @@ int main(int argc, char *argv[])
     while (scanf("%d %d", &box, &dim) == 2) {
         int i;
         int j;
+        int sol_size;
+        int sol_base;
 
         memset(data, 0, sizeof(data));
         for (i = 0; i < box; ++i) {
@@ -77,22 +87,41 @@ int main(int argc, char *argv[])
         qsort(&data, box, sizeof(data[0]), compare_size);
 
         for (i = 0; i < box; ++i) {
-            ++data[i].id;
             data[i].next = data[i].id;
-            data[i].size = 0;
+            data[i].size = 1;
         }
 
-        for (i = 1; i < box; ++i) {
-            for (j = i - 1; j >= 0; --j) {
-                if (is_stackable(&data[j], &data[i])) {
-                    if (data[j].size >= data[i].size) {
+        sol_base = 1;
+        sol_size = 1;
+
+        for (i = box - 2; i >= 0; --i) {
+            for (j = i + 1; j < box; ++j) {
+                if (is_stackable(&data[i], &data[j])) {
+                    if (data[i].size <= data[j].size) {
                         data[i].size = data[j].size + 1;
                         data[i].next = data[j].id;
                     }
                 }
+
+                if (data[i].size > sol_size) {
+                    sol_size = data[i].size;
+                    sol_base = data[i].id;
+                }
             }
         }
 
+        qsort(&data, box, sizeof(data[0]), compare_id);
+
+        printf("%d\n", sol_size);
+
+        while (data[sol_base].next != sol_base) {
+            printf("%d ", sol_base + 1);
+            sol_base = data[sol_base].next;
+        }
+        printf("%d\n", sol_base + 1);
+
+#if 0
+        printf("sol_size = %d, sol_base = %d\n", sol_size, sol_base);
         for (i = 0; i < box; ++i) {
             printf("id = %d, next = %d, size = %d, ", data[i].id, data[i].next, data[i].size);
             for (j = 0; j < dim; ++j) {
@@ -100,8 +129,7 @@ int main(int argc, char *argv[])
             }
             printf("\n");
         }
-
+#endif
     }
-
     return 0;
 }
