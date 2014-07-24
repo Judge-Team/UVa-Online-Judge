6f00ed8adf9b264aaa7d2ab259763c0f1ae99bea
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2014-07-24 21:33:18 +0800

    Add 10684

---
+++ src/czchen-10684.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int max(int x, int y)
+{
+    return x > y ? x : y;
+}
+
+int main(int argc, char *argv[])
+{
+    int count;
+    int i;
+
+    while (scanf("%d", &count) == 1) {
+        int i;
+
+        int current_max = -1;
+        int right_max = -1;
+        int x;
+
+        if (count == 0) {
+            break;
+        }
+
+        for (i = 0; i < count; ++i) {
+            scanf("%d", &x);
+
+            right_max += x;
+            if (right_max < 0) {
+                right_max = 0;
+            }
+
+            current_max = max(current_max, right_max);
+        }
+
+        if (current_max > 0) {
+            printf("The maximum winning streak is %d.\n", current_max);
+        } else {
+            printf("Losing streak.\n");
+        }
+    }
+
+    return 0;
+}
