3788047821257705cea5d78b4708c67ea8dde6bc
ZSShen <andy780328@hotmail.com>
2014-07-27 03:44:23 -0700

    The AC version of UVa#10154.

--- src/zsshen-10154.c
+++ src/zsshen-10154.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 
-#define MAX_STRENGTH    2147483647
 #define MAX_OBJECT      5608
 #define Min(a,b)        ((a) < (b) ? (a) : (b))
 
@@ -10,15 +9,16 @@
 typedef struct _Turtle {
     int m_weight;
     int m_strength;
+    int m_power;
 } Turtle;
 
 
 int compare(const void *p_src, const void *p_tge) {
     
-    if (((Turtle*)p_src)->m_strength == ((Turtle*)p_tge)->m_strength)
+    if (((Turtle*)p_src)->m_power == ((Turtle*)p_tge)->m_power)
         return 0;
     else {
-        if (((Turtle*)p_src)->m_strength < ((Turtle*)p_tge)->m_strength)
+        if (((Turtle*)p_src)->m_power > ((Turtle*)p_tge)->m_power)
             return 1;
         else
             return -1;
@@ -29,38 +29,50 @@ int compare(const void *p_src, const void *p_tge) {
 int main() {
     int i, j, idx, weight, power;
     int length, new_load;
-    int min_load[MAX_OBJECT];
-    Turtle list_turtle[MAX_OBJECT];
+    int load[MAX_OBJECT];
+    Turtle turtle[MAX_OBJECT];
     
     idx = 1;
     while(scanf("%d%d", &weight, &power) != EOF) {
-        list_turtle[idx].m_weight = weight;
-        list_turtle[idx].m_strength = power - weight;
+        turtle[idx].m_weight = weight;
+        turtle[idx].m_strength = power - weight;
+        turtle[idx].m_power = power;
         idx++;
     }  
     idx--;
 
-    /* Sort the turtles by their weights. */
-    qsort(list_turtle + 1, idx, sizeof(Turtle), compare);
+    /* Sort the turtles by their original strength. */
+    qsort(turtle + 1, idx, sizeof(Turtle), compare);
+
+    /**
+     * Let dp[len] denote the "minimum total weight" of tower with length "len".
+     *
+     * Given a turtle with index i:
+     *      1. Update the length of tower, if s[i] >= dp[len]:
+     *             len++;
+     *             dp[len] = dp[len - 1] + w[i].
+     *
+     *      2. And then maintain the optimal substructure.          
+     *             dp[j] = Min{dp[j], dp[j-1] + w[i]}, for 1 <= j <= len.  
+     *
+     */
 
     /* Initial step. */    
-    min_load[0] = MAX_STRENGTH;
-    min_load[1] = list_turtle[1].m_strength;
+    load[0] = 0;
+    load[1] = turtle[1].m_weight;
     length = 1;    
 
     /* Deduction step. */
     for (i = 2 ; i <= idx ; i++) {
-        if (list_turtle[i].m_weight <= min_load[length]) {
-            new_load = Min(list_turtle[i].m_strength, min_load[length] - list_turtle[i].m_weight);
-            min_load[++length] = new_load;
-        } else {
-            for (j = length - 1 ; j >= 0 ; j--) {
-                if (list_turtle[i].m_weight <= min_load[j]) {
-                    new_load = Min(list_turtle[i].m_strength, min_load[j] - list_turtle[i].m_weight);
-                    if (new_load > min_load[j + 1])
-                        min_load[j + 1] = new_load;
+        for (j = length ; j >= 0 ; j--) {
+            if (turtle[i].m_strength >= load[j]) {
+                if ((j + 1) > length) {
+                    load[j + 1] = load[j] + turtle[i].m_weight;
+                    length = j + 1;
+                } else {
+                    load[j + 1] = Min(load[j + 1], load[j] + turtle[i].m_weight);
                 }
-            }
+            }            
         }
     }
 
