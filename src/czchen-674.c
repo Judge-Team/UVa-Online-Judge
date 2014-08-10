a8030bedcaa8cd9471346dcd62a62837cd9c25dc
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2014-08-10 20:16:10 +0800

    Implement czchen-674 using bottom-up approach

--- src/czchen-674.c
+++ src/czchen-674.c
@@ -1,26 +1,71 @@
 #include <stdio.h>
 
-const int COIN_TYPE[] = { 50, 25, 10, 5, 1, 0 };
+const int COIN_TYPE[] = { 1, 5, 10, 25, 50 };
 
-static int find_change(int coin, const int *type)
-{
-    if (coin == 0) {
-        return 1;
-    } else if (coin < 0 || type[0] == 0) {
-        return 0;
-    } else {
-        return find_change(coin, type+1) + find_change(coin-type[0], type);
-    }
-}
+enum {
+    MAX_CENTS = 7489,
+    MAX_TYPES = sizeof(COIN_TYPE) / sizeof(COIN_TYPE[0]),
+};
 
 int main()
 {
     int coin;
-    int ans;
+    int ans[MAX_CENTS+1][MAX_TYPES];
+    int value;
+    int orig_value;
+    int type;
+    int x;
+
+    /*
+     * coin_type[i] = value of i-th coin. Start from 1 cent.
+     *
+     * change[v, i] = Types of change for valuen cents, using coin from 0 ~ i.
+     *
+     * change[v, i] = change[v, i-1] + change[v-coin_type[i], i] (if v-coin_type[i] >= 0)
+     *
+     * With the following initial value:
+     *
+     * coin[0, i] = 1
+     * coin[v, 0] = 1
+     */
+
+    for (type = 0; type < MAX_TYPES; ++type) {
+        ans[0][type] = 1;
+    }
+
+    for (value = 1; value <= COIN_TYPE[MAX_TYPES-1]; ++value) {
+        ans[value][0] = 1;
+    }
+
+    for (type = 1; type < MAX_TYPES; ++type) {
+        for (value = 1; value <= COIN_TYPE[MAX_TYPES-1]; ++value) {
+            ans[value][type] = ans[value][type-1];
+
+            x = value - COIN_TYPE[type];
+            if (x >= 0) {
+                ans[value][type] += ans[x][type];
+            }
+        }
+    }
 
     while (scanf("%d", &coin) == 1) {
-        ans = find_change(coin, COIN_TYPE);
-        printf("%d\n", ans);
+        if (value < coin) {
+            orig_value = value;
+
+            for (; value <= coin; ++value) {
+                ans[value][0] = 1;
+            }
+
+            for (type = 1; type < MAX_TYPES; ++type) {
+                for (value = orig_value; value <= coin; ++value) {
+                    ans[value][type] = ans[value][type-1] + ans[value-COIN_TYPE[type]][type];
+                }
+            }
+
+            value = coin;
+        }
+
+        printf("%d\n", ans[coin][MAX_TYPES-1]);
     }
 
     return 0;
