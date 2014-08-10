a2a978c223266b9668e1e62fc34cdc930614cb47
ZSShen <andy780328@hotmail.com>
2014-08-10 05:52:49 -0700

    Andy: Add the recursive formula for #825.

--- src/zsshen-825.c
+++ src/zsshen-825.c
@@ -41,6 +41,15 @@ int main() {
             }        
         }
 
+        /**
+         * Recursive formula:
+         * 1. dp[i][j] : The number of ways to reach location (i, j).
+         * 2. if map[i][j] == false, dp[i][j] = 0.
+         *    else dp[i][j] = | dp[i - 1][j] + dp[i][j - 1], if i > 1 and j > 1.
+         *                    | dp[i - 1][j]               , if j == 1.
+         *                    | dp[i][j - 1]               , if i == 1.
+         */
+
         dp[1][1] = 1;
         for (i = 1 ; i <= num_row ; i++) {
             for (j = 1 ; j <= num_col ; j++) {
@@ -55,14 +64,6 @@ int main() {
             }
         }
 
-        /*
-        for (i = 1 ; i <= num_row ; i++) {
-            for (j = 1 ; j <= num_col ; j++) {
-                printf("(%3d:%3d) ", map[i][j], dp[i][j]);
-            }
-            printf("\n");
-        }
-        */
         printf("%llu\n", dp[num_row][num_col]);
 
         round--;
