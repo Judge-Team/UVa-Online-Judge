713c7d8659d670d3c227fc0db4aa11ef986f0422
ZSShen <andy780328@hotmail.com>
2014-10-03 08:11:53 -0700

    Andy: I think it is the optimized version using trie. But TLE!

--- src/zsshen-11362.c
+++ src/zsshen-11362.c
@@ -4,6 +4,126 @@
 #include <string.h>
 
 
+/*------------------------------------------------------------*/
+/*                   Optimized version                        */
+/*------------------------------------------------------------*/
+#define COUNT_BRANCH    10
+#define BUF_SIZE        10000
+#define ASCII_BIAS      48
+
+
+typedef struct _Node {
+    struct _Node *succ[COUNT_BRANCH];
+    bool   is_leaf;
+} Node;
+
+
+void init_trie(Node**);
+
+
+void deinit_trie(Node*);
+
+
+void construct_trie(Node*, int);
+
+
+int main() {
+    int  rc, count_case, size_book;
+    Node *trie;
+
+    rc = scanf("%d", &count_case);
+    while (count_case) {
+        rc = scanf("%d", &size_book);
+
+        init_trie(&trie);        
+        construct_trie(trie, size_book);
+        deinit_trie(trie);
+
+        count_case--;
+    }
+
+    return 0;
+}
+
+
+void init_trie(Node **p_trie) {
+    int  i;
+    Node *trie;
+
+    *p_trie = (Node*)malloc(sizeof(Node));
+    trie = *p_trie;    
+    for (i = 0 ; i < COUNT_BRANCH ; i++) {
+        trie->succ[i] = NULL;        
+    }        
+    trie->is_leaf = true;
+
+    return;
+}
+
+
+void deinit_trie(Node *trie) {
+    int i;
+
+    if (trie != NULL) {    
+        for (i = 0 ; i < COUNT_BRANCH ; i++) {
+            if (trie->succ[i] != NULL) {
+                deinit_trie(trie->succ[i]);
+            }    
+        }
+        free(trie);
+    }
+
+    return;
+}
+
+
+void construct_trie(Node *trie, int size_book) {
+    int  rc, i, idx_phone, len_phone, number;
+    Node *curr, *succ;
+    char buf[BUF_SIZE + 1];
+
+    for (idx_phone = 0 ; idx_phone < size_book ; idx_phone++) {
+        memset(buf, 0, sizeof(char) * (BUF_SIZE + 1));
+        rc = scanf("%s", buf);
+        len_phone = strlen(buf);
+
+        curr = trie;
+        for (i = 0 ; i < len_phone ; i++) {
+            number = buf[i] - ASCII_BIAS;
+            if (curr->succ[number] == NULL) {
+                break;
+            }
+            curr = curr->succ[number];
+
+            /* Some of the previously recorded string is the prefix of the current string. */
+            if (curr->is_leaf == true) {
+                printf("NO\n");
+                return;
+            }
+        }
+
+        /* The current string is the prefix of some previously recorded string. */
+        if ((i == len_phone) && (curr->is_leaf == false)) {
+            printf("NO\n");
+            return;
+        }
+
+        for (; i < len_phone ; i++) {
+            number = buf[i] - ASCII_BIAS;                
+            init_trie(&succ);
+            curr->succ[number] = succ;
+            curr->is_leaf = false;
+            curr = curr->succ[number];
+        }
+    }
+
+    printf("YES\n");
+    return;
+}
+/*------------------------------------------------------------*/
+/*                    Brute force version                     */
+/*------------------------------------------------------------*/
+/*
 #define BUF_SIZE 10000
 
 
@@ -81,11 +201,11 @@ void scan_phone_book(Phone *phone_book, int size_book) {
     int idx_pred, idx_curr, i;
     bool not_match;
 
-    /* First sort the phone book with the lengh of each phone string. */
+    // First sort the phone book with the lengh of each phone string.
     qsort(phone_book, size_book, sizeof(Phone), func_sort_phone_book);
 
-    /* Apply backward comparsion to determine if there is string in the previous list
-     * which is the prefix of the current string.  */
+    // Apply backward comparsion to determine if there is string in the previous list
+    // which is the prefix of the current string.
     for (idx_curr = 1 ; idx_curr < size_book ; idx_curr++) {
         for (idx_pred = idx_curr - 1 ; idx_pred >= 0 ; idx_pred--) {
             not_match = false;
@@ -116,4 +236,4 @@ int func_sort_phone_book(const void *src, const void *tge) {
 
     return phone_src->len_phone - phone_tge->len_phone;
 }
-
+*/
