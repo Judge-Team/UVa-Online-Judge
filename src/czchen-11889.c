05201def2270cbcf2b45658fdc0b88d68119efdd
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2014-10-16 18:20:09 +0800

    Fix czchen-10889

--- src/czchen-11889.c
+++ src/czchen-11889.c
@@ -1,21 +1,58 @@
 #include <stdio.h>
 
+static int gcd(int a, int b)
+{
+    return b == 0 ? a : gcd(b, a%b);
+}
+
 int main()
 {
     int count;
-    int x;
-    int lcm;
+
+    int a;
+    int b;
+    int c;
+
+    int factor;
+    int g;
 
     scanf("%d", &count);
 
     for (; count > 0; --count) {
-        scanf("%d%d", &x, &lcm);
+        scanf("%d%d", &a, &c);
 
-        if (lcm % x) {
+        /*
+         * lcm(A, B) = C
+         *
+         * To calculate B, we first assume B = C / A. If GCD(A, B) == 1, B is
+         * indeed C / A.
+         *
+         * However, if GCD(A, B) > 1, we know that some factors are both in A
+         * and B, and B has higher degree in those factors.
+         *
+         * To find the factors, we use A' = A / gcd(A, B) to remove all factors
+         * in A. Repeating do so we can finally remove all factors in A and use
+         * that factor to find real B.
+         */
+
+        if (c % a) {
             printf("NO SOLUTION\n");
-        } else {
-            printf("%d\n", lcm/x);
+            continue;
         }
+
+        b = c / a;
+
+        factor = 1;
+        g = gcd(a, b);
+
+        while(g != 1)
+        {
+            factor *= g;
+            a /= g;
+            g = gcd(a, b);
+        }
+
+        printf("%d\n", b * factor);
     }
 
     return 0;
