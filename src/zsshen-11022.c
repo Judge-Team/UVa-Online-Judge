babbf8979231fb451f10a013c5aff203cae407cd
ZSShen <andy780328@hotmail.com>
2015-01-23 08:47:31 -0800

    Andy: Add the recursive definition of UVa#11022.

--- src/zsshen-11022.c
+++ src/zsshen-11022.c
@@ -9,12 +9,19 @@
 #define LEFT_DRIVE      0
 #define RIGHT_DRIVE     1
 
+/*
+ * Time Complexity  : O(n^4)
+ * Recursive Formula:
+ * dp[i][j] = MIN    | a[i][k]             , if (str[i, k])+ eq str[k+1, j]
+ *            i<=k<j | a[i][k] + a[k+1][j] , else
+ */
+
 
 void factor_string(char *sz)
 {
     int dp[BUF_SIZE][BUF_SIZE];
     int len_sz = strlen(sz);
-    
+
     int range;
     for (range = 0 ; range < len_sz ; range++)
         dp[range][range] = 1;
@@ -38,8 +45,8 @@ void factor_string(char *sz)
                     direction = LEFT_DRIVE;
                 }
                 if ((len_large % len_small) == 0) {
+                    idx_small = idx_large = 0;
                     if (direction == LEFT_DRIVE) {
-                        idx_small = idx_large = 0;
                         while (idx_large < len_large) {
                             if (sz[i + idx_small] != sz[k + 1 + idx_large]) {
                                 check = false;
@@ -51,7 +58,6 @@ void factor_string(char *sz)
                                 idx_small = 0;
                         }
                     } else {
-                        idx_small = idx_large = 0;
                         while (idx_large < len_large) {
                             if (sz[i + idx_large] != sz[k + 1 + idx_small]) {
                                 check = false;
@@ -65,10 +71,6 @@ void factor_string(char *sz)
                     }
                 } else
                     check = false;
-                /*
-                printf("dp[%2d][%2d] = %2d\tdp[%2d][%2d] = %2d\tcheck%d\n",
-                        i + 1, k + 1, dp[i][k], k + 2, j + 1, dp[k + 1][j], check);
-                */
                 if (check)
                     val_tmp = dp[i][k];
                 else
@@ -77,10 +79,9 @@ void factor_string(char *sz)
                     val_min = val_tmp;
             }
             dp[i][j] = val_min;
-            /* printf("dp[%2d][%2d] = %2d\n", i + 1, j + 1, dp[i][j]); */
         }
     }
-    
+
     printf("%d\n", dp[0][len_sz - 1]);
     return;
 }
