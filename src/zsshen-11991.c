763671f373119eaaed4d2c4f4092d807280dc994
ZSShen <andy780328@hotmail.com>
2015-02-10 05:33:17 -0800

    Andy: AC code for UVa#11991 (implemented by vector)

--- src/zsshen-11991.c
+++ src/zsshen-11991.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 #include <string.h>
 
-
+/*
 #define MAX_ELEMENT         1000000
 #define DIRECTION_LEFT      0
 #define DIRECTION_RIGHT     1
@@ -184,3 +184,94 @@ int main()
 
     return 0;
 }
+*/
+
+#define MAX_COUNT_ELEMENT       (1000000)
+#define FACTOR_RESIZE           (2)
+#define INIT_CAPACITY           (2)
+
+
+typedef struct _VECTOR {
+    int size;
+    int capacity;
+    int *buffer;
+} VECTOR;
+
+
+void vector_push_back(VECTOR *vec, int ofst_occur)
+{
+    if (vec->size == vec->capacity) {
+        int new_capacity = vec->capacity * FACTOR_RESIZE;
+        vec->buffer = (int*)realloc(vec->buffer, sizeof(int) * new_capacity);
+        vec->capacity = new_capacity;
+    }
+
+    vec->buffer[vec->size] = ofst_occur;
+    vec->size++;
+
+    return;
+}
+
+
+void vector_new(VECTOR **p_vec)
+{
+    *p_vec = (VECTOR*)malloc(sizeof(VECTOR));
+    (*p_vec)->buffer = (int*)malloc(sizeof(int) * INIT_CAPACITY);
+    (*p_vec)->size = 0;
+    (*p_vec)->capacity = INIT_CAPACITY;
+
+    return;
+}
+
+
+void vector_delete(VECTOR *vec)
+{
+    if (vec) {
+        free(vec->buffer);
+        free(vec);
+    }
+
+    return;
+}
+
+
+int main()
+{
+    VECTOR **arr_vec = (VECTOR**)malloc(sizeof(VECTOR*) * (MAX_COUNT_ELEMENT + 1));
+
+    int num_element, num_query;
+    while (scanf("%d%d", &num_element, &num_query) != EOF) {
+        int idx;
+        for (idx = 1 ; idx <= MAX_COUNT_ELEMENT ; idx++)
+            arr_vec[idx] = NULL;
+
+        for (idx = 1 ; idx <= num_element ; idx++) {
+            int element, dummy;
+            dummy = scanf("%d", &element);
+            if (!arr_vec[element])
+                vector_new(&(arr_vec[element]));
+            vector_push_back(arr_vec[element], idx);
+        }
+
+        for (idx = 1 ; idx <= num_query ; idx++) {
+            int element, time_occur, dummy;
+            dummy = scanf("%d%d", &time_occur, &element);
+            if (arr_vec[element]) {
+                if (time_occur <= arr_vec[element]->size)
+                    printf("%d\n", arr_vec[element]->buffer[time_occur - 1]);
+                else
+                    printf("0\n");
+            } else
+                printf("0\n");
+        }
+
+        for (idx = 1 ; idx <= MAX_COUNT_ELEMENT ; idx++)
+            vector_delete(arr_vec[idx]);
+    }
+
+    free(arr_vec);
+
+    return 0;
+}
+
+
