7d9b98c0a785b4ba1b0f1d2f579e7ba44d2b4781
ZSShen <andy780328@hotmail.com>
2015-02-15 04:20:48 -0800

    Andy: AC code for UVa#12532.

--- src/zsshen-12532.c
+++ src/zsshen-12532.c
@@ -7,88 +7,102 @@
 #define FLAG_POSITIVE   (1)
 #define FLAG_NEGATIVE   (-1)
 #define FLAG_ZERO       (0)
-#define BUF_SIZE        (1024)
-
-
-#define LEFT(node)      (node << 1)
-#define RIGHT(node)     (node << 1 | 1)
-#define TOTAL(node)     ((node + 1) * node / 2 + 1)
 
 
 typedef struct _NODE {
     int end_left;
     int end_right;
-    char flag_value;
+    char flag;
+    struct _NODE *child_left;
+    struct _NODE *child_right;
 } NODE;
 
 
-int construct_tree(NODE *tree, int idx_node, int end_left, int end_right)
+char construct_tree(NODE **p_curr, int end_left, int end_right)
 {
     if (end_left == end_right) {
+        *p_curr = (NODE*)malloc(sizeof(NODE));
+        NODE *curr = *p_curr;
+
         int value, dummy;
         dummy = scanf("%d", &value);
         if (value == 0)
-            tree[idx_node].flag_value = FLAG_ZERO;
+            curr->flag = FLAG_ZERO;
         else {
             if (value > 0)
-                tree[idx_node].flag_value = FLAG_POSITIVE;
+                curr->flag = FLAG_POSITIVE;
             else
-                tree[idx_node].flag_value = FLAG_NEGATIVE;    
+                curr->flag = FLAG_NEGATIVE;
         }
-        tree[idx_node].end_left = end_left;
-        tree[idx_node].end_right = end_right;
-        return tree[idx_node].flag_value;
+
+        curr->end_left = end_left;
+        curr->end_right = end_right;
+        curr->child_left = curr->child_right = NULL;
+        return curr->flag;
     }
 
+    *p_curr = (NODE*)malloc(sizeof(NODE));
+    NODE *curr = *p_curr;
+    NODE *child_left, *child_right;
+
     int idx_mid = (end_left + end_right) / 2;
-    int child_left = LEFT(idx_node);
-    int child_right = RIGHT(idx_node);
-    tree[idx_node].flag_value = construct_tree(tree, child_left, end_left, idx_mid) *
-                                construct_tree(tree, child_right, (idx_mid + 1), end_right);
-    tree[idx_node].end_left = end_left;
-    tree[idx_node].end_right = end_right;
-    return tree[idx_node].flag_value;                            
+    curr->flag = construct_tree(&child_left, end_left, idx_mid) *
+                 construct_tree(&child_right, (idx_mid + 1), end_right);
+
+    curr->end_left = end_left;
+    curr->end_right = end_right;
+    curr->child_left = child_left;
+    curr->child_right = child_right;
+
+    return curr->flag;
 }
 
 
-void change_node_flag(NODE *tree, int idx_node, int idx_element, char flag_value)
+void destruct_tree(NODE *curr)
 {
-    if (tree[idx_node].end_left == idx_element &&
-        tree[idx_node].end_right == idx_element) {
-        tree[idx_node].flag_value = flag_value;
-        return;
+    if (curr) {
+        destruct_tree(curr->child_left);
+        destruct_tree(curr->child_right);
+        free(curr);
     }
 
-    int idx_mid = (tree[idx_node].end_left + tree[idx_node].end_right) / 2;
-    int child_left = LEFT(idx_node);
-    int child_right = RIGHT(idx_node);
-    if (idx_element <= idx_mid)
-        change_node_flag(tree, child_left, idx_element, flag_value);
-    else
-        change_node_flag(tree, child_right, idx_element, flag_value);
-
-    tree[idx_node].flag_value = tree[child_left].flag_value *
-                                tree[child_right].flag_value;
     return;
 }
 
 
-char query_range_product(NODE *tree, int idx_node, int range_left, int range_right)
+void change_node_flag(NODE *curr, int idx_element, char flag)
 {
-    if (tree[idx_node].end_left == range_left &&
-        tree[idx_node].end_right == range_right)
-        return tree[idx_node].flag_value;
+    if ((curr->end_left == idx_element) &&
+        (curr->end_right == idx_element)) {
+        curr->flag = flag;
+        return;
+    }
 
-    int idx_mid = (tree[idx_node].end_left + tree[idx_node].end_right) / 2;
-    int child_left = LEFT(idx_node);
-    int child_right = RIGHT(idx_node);
-    if (range_right <= idx_mid)
-        return query_range_product(tree, child_left, range_left, range_right);
-    else if (range_left > idx_mid)
-        return query_range_product(tree, child_right, range_left, range_right);
+    int idx_mid = (curr->end_left + curr->end_right) / 2;
+    if (idx_element <= idx_mid)
+        change_node_flag(curr->child_left, idx_element, flag);
     else
-        return query_range_product(tree, child_left, range_left, idx_mid) *
-               query_range_product(tree, child_right, (idx_mid + 1), range_right);
+        change_node_flag(curr->child_right, idx_element, flag);
+
+    curr->flag = curr->child_left->flag * curr->child_right->flag;
+    return;
+}
+
+
+char query_range_product(NODE *curr, int range_left, int range_right)
+{
+    if ((curr->end_left == range_left) &&
+        (curr->end_right == range_right))
+        return curr->flag;
+
+    int idx_mid = (curr->end_left + curr->end_right) / 2;
+    if (range_right <= idx_mid)
+        return query_range_product(curr->child_left, range_left, range_right);
+    else if (range_left > idx_mid)
+        return query_range_product(curr->child_right, range_left, range_right);
+    else
+        return query_range_product(curr->child_left, range_left, idx_mid) *
+               query_range_product(curr->child_right, (idx_mid + 1), range_right);
 }
 
 
@@ -97,9 +111,12 @@ int main()
     int cnt_element, cnt_query;
 
     while (scanf("%d%d", &cnt_element, &cnt_query) != EOF) {
-        NODE *tree = (NODE*)malloc(sizeof(NODE) * TOTAL(cnt_element));
-        construct_tree(tree, 1, 1, cnt_element);
 
+        /* Read the array of elements and construct the interval tree. */
+        NODE *tree;
+        construct_tree(&tree, 1, cnt_element);
+
+        /* Update the tree values and acquire the range properties. */
         int idx, dummy;
         char absorb;
         dummy = scanf("%c", &absorb);
@@ -109,21 +126,21 @@ int main()
             if (command == 'C') {
                 int idx_element, value;
                 dummy = scanf("%d%d%c", &idx_element, &value, &absorb);
-                char flag_value;
+                char flag;
                 if (value == 0)
-                    flag_value = FLAG_ZERO;
+                    flag = FLAG_ZERO;
                 else {
                     if (value > 0)
-                        flag_value = FLAG_POSITIVE;
+                        flag = FLAG_POSITIVE;
                     else
-                        flag_value = FLAG_NEGATIVE;    
+                        flag = FLAG_NEGATIVE;    
                 }
-                change_node_flag(tree, 1, idx_element, flag_value);
+                change_node_flag(tree, idx_element, flag);
             } else {
                 int range_left, range_right;
                 dummy = scanf("%d%d%c", &range_left, &range_right, &absorb);
-                char flag_result = query_range_product(tree, 1, range_left, range_right);
-                switch(flag_result) {
+                char flag = query_range_product(tree, range_left, range_right);
+                switch(flag) {
                   case FLAG_POSITIVE:
                     printf("+");
                     break;
@@ -138,7 +155,7 @@ int main()
         }
         printf("\n");
 
-        free(tree);
+        destruct_tree(tree);
     }
 
     return 0;
